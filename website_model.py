90bd2bf777a25ba52ace0a448c861d19ed3e4e5a
susiep <32068626+susiep@users.noreply.github.com>
2017-11-29 12:51:20 +0000

    Add files via upload

--- website_model.py
+++ website_model.py
@@ -13,27 +13,30 @@ import matplotlib.animation
 import agentframework
 import csv
 
-#Creating the environment
-environment = []                                        #Make empty environment list
+#path to ffmpeg needed to save animation
+matplotlib.pyplot.rcParams['animation.ffmpeg_path'] = '/usr/local/bin/ffmpeg'
+
+#Creating the orchard environment
+orchard = []                                            #Make empty orchard environment list
 f = open('in.txt', newline='')                          #open in the in.txt file as f
 reader = csv.reader(f, quoting=csv.QUOTE_NONNUMERIC)    #read in f as a csv called reader
 for row in reader:                                      #For every row in reader
     rowlist = []                                        #Create a new list
     for value in row:                                   #For every value in row
         rowlist.append(value)                           #Add the value to the row list
-    environment.append(rowlist)                         #When finished, append all the row lists to the environment list
+    orchard.append(rowlist)                             #When finished, append all the row lists to the environment list
 f.close()                                               #Close the file
 
 #Define model parameters
-num_of_agents = 10                                      #Define number of agents
+num_of_people = 10                                      #Define number of people
 num_of_iterations = 100                                 #Define number of model iterations
-neighbourhood = 30                                      #Define distance which agents can interact with each other
+neighbourhood = 30                                      #Define distance which people can interact with each other
 
 
 #Command line arguments
 #Allows model to be run from the command line by specifying the 3 parameters
-sys.argv.extend([num_of_agents, num_of_iterations, neighbourhood])
-print ("Model\n Number of agents: %s\n Number of iterations: %s\n Neighbourhood size: %s"% (sys.argv[1], sys.argv[2], sys.argv[3])) 
+sys.argv.extend([num_of_people, num_of_iterations, neighbourhood])
+print ("Model\n Number of people: %s\n Number of iterations: %s\n Neighbourhood size: %s"% (sys.argv[1], sys.argv[2], sys.argv[3])) 
 
 #Defining animation result parameters
 fig =  matplotlib.pyplot.figure(figsize=(7, 7))         #Create the figure, called fig
@@ -41,9 +44,9 @@ matplotlib.pyplot.xlim(xmax = 300)                      #Define the size of the 
 matplotlib.pyplot.ylim(ymax = 300)                      #Define the size of the y axis
 
 #Creating the agents
-agents = []                                                     #Make empty agent list       
-for i in range (num_of_agents):                                 #For specified number of agents
-    agents.append(agentframework.Agent(environment, agents))    #Create an agent from agent framework and append to the list
+people = []                                                     #Make empty people list       
+for i in range (num_of_people):                                 #For specified number of people
+    people.append(agentframework.Person(orchard, people))       #Create an person from the framework and append to the list
     
 
 #Instructions for every animation frame
@@ -52,18 +55,18 @@ def update(frame_number):                                       #For each update
     matplotlib.pyplot.xlim(xmax = 300)                          #Reset the x axis to 300
     matplotlib.pyplot.ylim(ymax = 300)                          #Reset the y axis to 300
     for j in range(num_of_iterations):                          #For every iteration
-        random.shuffle(agents, random = None)                   #Shuffle the order the agents are processed
-        for i in range (num_of_agents):                         #For every agent
-            agents[i].move()                                    #Move the agent
-            agents[i].eat()                                     #Make them eat
-            agents[i].share_with_neighbours(neighbourhood)      #Allow them to share with their neighbours
-            agents[i].vom()                                     #Allow them to throw up           
-    matplotlib.pyplot.imshow(environment)                       #Show the environment
-    for i in range(num_of_agents):                              #For every agent load into animation frame
-            matplotlib.pyplot.scatter(agents[i].x,agents[i].y, color = 'lightblue')
+        random.shuffle(people, random = None)                   #Shuffle the order the people are processed
+        for i in range (num_of_people):                         #For every person
+            people[i].move()                                    #Move the person
+            people[i].pick_fruit()                               #Make them eat
+            people[i].share_with_neighbours(neighbourhood)      #Allow them to share with their neighbours
+            people[i].drop()                                    #Cultivate the orchard           
+    matplotlib.pyplot.imshow(orchard)                           #Show the orchard environment
+    for i in range(num_of_people):                              #For every person load into animation frame
+            matplotlib.pyplot.scatter(people[i].x,people[i].y, color = 'lightblue')
 
 #Run the animation
 animation = matplotlib.animation.FuncAnimation(fig, update, interval=1, repeat=False, frames=num_of_iterations)
 fig.show()
-
-
+#Save the animation
+animation.save('an.mp4', writer='imagemagick', dpi= 80, fps=5)
