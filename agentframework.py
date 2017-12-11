b94311f0c7f7aa863d074c5726425506acd7a157
susiep <32068626+susiep@users.noreply.github.com>
2017-12-11 15:16:40 +0000

    Update agentframework.py

--- agentframework.py
+++ agentframework.py
@@ -6,14 +6,14 @@ import random
 
 #Create the person class
 class Person():                                         #Create the class called Person
-    def __init__ (self, orchard, people):           #Give each person attributes for...
-        self._x = None                #x location (random)
-        self._y = None                  #y location (random)
+    def __init__ (self, orchard, people):               #Give each person attributes for...
+        self._x = None                                  #x location (random)
+        self._y = None                                  #y location (random)
         self.orchard = orchard                          #orchard
-        self.apples = 0                            #Store of apples
+        self.apples = 0                                 #Store of apples
         self.people = people                            #Information about the other agents.
         
-    def getx(self):
+    def getx(self):                                     #Use set and get property functions to safeguard the x,y variables
         return self._x
     
     def setx(self):
@@ -25,32 +25,33 @@ class Person():                                         #Create the class called
     def sety(self):
         self._y = random.randint(0,300)
         
-    def __str__(self):                                  #When an person is printed, return information about their position and no of apples
+    def __str__(self):                                  
+   #When an person is printed, return information about their position and no of apples
         return ("At position ( %d , %d ) and has %d apples " % (self.x, self.y,self.apples))
     
-    def move(self):                                     #Define the move function
-        if self.apples < 100:                           #If they have collected less than 100 apples then they move quicker
+    def move(self):                                         #Define the move function
+        if self.apples < 100:                               #If they have collected less than 100 apples then they move quicker
             if random.random() < 0.5:
-                self._x = (self._x + 3) % 300             #Create a random motion of 3 spaces in x 
+                self._x = (self._x + 3) % 300               #Create a random motion of 3 spaces in x 
             else:
                 self._x = (self._x - 3) % 300
-            if random.random() < 0.5:                   #Create a random motion of 3 spaces in y 
+            if random.random() < 0.5:                       #Create a random motion of 3 spaces in y 
                 self._y = (self._y + 3) % 300
             else:
                 self._y = (self._y - 3) % 300
-        else:                                           #If they have collected more than 100 they move slower
+        else:                                               #If they have collected more than 100 they move slower
             if random.random() < 0.5:
-                self._x = (self._x + 1) % 300             #Create a random motion of 1 space in x 
+                self._x = (self._x + 1) % 300               #Create a random motion of 1 space in x 
             else:
                 self._x = (self._x - 1) % 300
-            if random.random() < 0.5:                   #Create a random motion of 1 space in y
+            if random.random() < 0.5:                       #Create a random motion of 1 space in y
                 self._y = (self._y + 1) % 300
             else:
                 self._y = (self._y - 1) % 300
             
     def pick_fruit(self):                                       #Define the pick_fruit function
-        if self.orchard[self._y][self._x] > 5:                    #When the tree has more than 5 apples
-            self.orchard[self._y][self._x] -=5                #Take 5 apples from the tree
+        if self.orchard[self._y][self._x] > 5:                  #When the tree has more than 5 apples
+            self.orchard[self._y][self._x] -=5                  #Take 5 apples from the tree
             self.apples += 5                                     
         else:                                                   #When the tree has less than 5 apples
             self.apples += self.orchard[self._y][self._x]       #Pick all the apples
@@ -69,11 +70,11 @@ class Person():                                         #Create the class called
                 self.apples = average
                 person.apples = average
     
-    def drop (self):                                            #Define the drop function
-         if self.apples > 140:                                  #If the agent has more than 140 apples
+    def drop (self):                                                #Define the drop function
+         if self.apples > 140:                                      #If the agent has more than 140 apples
              apples_dropped = random.randint(20,100)            
-             self.apples -=  apples_dropped                     #They drop between 20 and 100 apples
-             self.orchard[self._y][self._x] += apples_dropped     #And give some back to the environment
+             self.apples -=  apples_dropped                         #They drop between 20 and 100 apples
+             self.orchard[self._y][self._x] += apples_dropped       #And give some back to the environment
              
 
             
